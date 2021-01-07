1fe19fc449905fecf4ff490571717577d86a30d7
Mohammad M Rahman <45700103+mabmahmud@users.noreply.github.com>
2021-01-06 23:17:31 -0400

    Add files via upload

---
+++ LifeInsuranceApp/CIS_2225_T2_Rahman_Mohammad/frmCalculateCommission.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace CIS_2225_T2_Rahman_Mohammad
+{
+    partial class frmCalculateCommission
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFName = new System.Windows.Forms.Label();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.lblInsuranceAmount = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtInsuranceAmount = new System.Windows.Forms.TextBox();
+            this.btnCalculatrCommission = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSummary = new System.Windows.Forms.Button();
+            this.btnAllClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFName.Location = new System.Drawing.Point(15, 40);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(81, 18);
+            this.lblFName.TabIndex = 0;
+            this.lblFName.Text = "First Nmae";
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLName.Location = new System.Drawing.Point(15, 71);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(80, 18);
+            this.lblLName.TabIndex = 1;
+            this.lblLName.Text = "Last Nmae";
+            // 
+            // lblInsuranceAmount
+            // 
+            this.lblInsuranceAmount.AutoSize = true;
+            this.lblInsuranceAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInsuranceAmount.Location = new System.Drawing.Point(15, 104);
+            this.lblInsuranceAmount.Name = "lblInsuranceAmount";
+            this.lblInsuranceAmount.Size = new System.Drawing.Size(127, 18);
+            this.lblInsuranceAmount.TabIndex = 2;
+            this.lblInsuranceAmount.Text = "Insurance Amount";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(183, 36);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(182, 24);
+            this.txtFirstName.TabIndex = 3;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(183, 69);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(182, 24);
+            this.txtLastName.TabIndex = 4;
+            // 
+            // txtInsuranceAmount
+            // 
+            this.txtInsuranceAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInsuranceAmount.Location = new System.Drawing.Point(183, 101);
+            this.txtInsuranceAmount.Name = "txtInsuranceAmount";
+            this.txtInsuranceAmount.Size = new System.Drawing.Size(182, 24);
+            this.txtInsuranceAmount.TabIndex = 5;
+            // 
+            // btnCalculatrCommission
+            // 
+            this.btnCalculatrCommission.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculatrCommission.Location = new System.Drawing.Point(83, 149);
+            this.btnCalculatrCommission.Name = "btnCalculatrCommission";
+            this.btnCalculatrCommission.Size = new System.Drawing.Size(201, 28);
+            this.btnCalculatrCommission.TabIndex = 6;
+            this.btnCalculatrCommission.Text = "Calculate";
+            this.btnCalculatrCommission.UseVisualStyleBackColor = true;
+            this.btnCalculatrCommission.Click += new System.EventHandler(this.btnCalculatrCommission_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(145, 207);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(113, 28);
+            this.btnClear.TabIndex = 7;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSummary
+            // 
+            this.btnSummary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSummary.Location = new System.Drawing.Point(18, 207);
+            this.btnSummary.Name = "btnSummary";
+            this.btnSummary.Size = new System.Drawing.Size(113, 28);
+            this.btnSummary.TabIndex = 8;
+            this.btnSummary.Text = "Sumamry";
+            this.btnSummary.UseVisualStyleBackColor = true;
+            this.btnSummary.Click += new System.EventHandler(this.btnSummary_Click);
+            // 
+            // btnAllClear
+            // 
+            this.btnAllClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAllClear.Location = new System.Drawing.Point(274, 207);
+            this.btnAllClear.Name = "btnAllClear";
+            this.btnAllClear.Size = new System.Drawing.Size(113, 28);
+            this.btnAllClear.TabIndex = 9;
+            this.btnAllClear.Text = "All Clear";
+            this.btnAllClear.UseVisualStyleBackColor = true;
+            this.btnAllClear.Click += new System.EventHandler(this.btnAllClear_Click);
+            // 
+            // frmCalculateCommission
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 327);
+            this.Controls.Add(this.btnAllClear);
+            this.Controls.Add(this.btnSummary);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculatrCommission);
+            this.Controls.Add(this.txtInsuranceAmount);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.lblInsuranceAmount);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.lblFName);
+            this.Name = "frmCalculateCommission";
+            this.Text = "Calculate Commission";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.Label lblInsuranceAmount;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtInsuranceAmount;
+        private System.Windows.Forms.Button btnCalculatrCommission;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSummary;
+        private System.Windows.Forms.Button btnAllClear;
+    }
+}
+
---
+++ LifeInsuranceApp/CIS_2225_T2_Rahman_Mohammad/frmCalculateCommission.cs
@@ -0,0 +1,128 @@
+﻿/*
+  Progam name:  Life Insurance Application
+  Description:  Life insurance policy that calculates sales's person's commissin based on each sales as well all a summary for the total system with extended functionalities
+  Date:          02 October, 2020
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS_2225_T2_Rahman_Mohammad
+{
+    public partial class frmCalculateCommission : Form
+    {
+        public frmCalculateCommission()
+        {
+            InitializeComponent();
+        }
+
+        double commissionEarned = 0.00;
+        public int countTotalSales = 0;
+
+        private int countTotalPolicies = 0;
+
+        // Button to show calculated commission
+        private void btnCalculatrCommission_Click(object sender, EventArgs e)
+        {
+            calculateCommissionRAte();
+        }
+
+        // Boolean method to calculate commission rate 
+        private bool calculateCommissionRAte()
+        {
+
+            double.TryParse(txtInsuranceAmount.Text, out double PolicyAmount);
+
+            if (validateGeneralFields())
+            {
+
+                if (PolicyAmount >= 0 && PolicyAmount < 1000)
+                {
+                    commissionEarned = PolicyAmount * 0.05;
+                    MessageBox.Show("Hello  " + txtFirstName.Text + " " + txtLastName.Text +
+                         " " + "The commission dollar earned " + commissionEarned + " commission rate is 5%");
+                    countTotalPolicies++;
+                }
+                if (PolicyAmount >= 1000 && PolicyAmount < 1000000)
+                {
+                    commissionEarned = PolicyAmount * 0.10;
+                    MessageBox.Show("Hello  " + txtFirstName.Text + " " + txtLastName.Text +
+                         " " + "The commission dollar earned " + commissionEarned + " commission rate is 10%");
+                    countTotalPolicies++;
+                }
+                if(PolicyAmount >= 1000000)
+                {
+                    commissionEarned = PolicyAmount * 0.20;
+                    MessageBox.Show("Hello  " + txtFirstName.Text + " " + txtLastName.Text +
+                         " " + "The commission dollar earned " + commissionEarned + " commission rate is 20%");
+                    countTotalPolicies++;
+                }
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Please enter first name, last name and policy amount.");
+                return false;
+
+            }
+
+
+        }
+
+        // Boolean method to varify the fields
+        private bool validateGeneralFields()
+        {
+            //Validate general fields
+            if (txtFirstName.Text == "")
+            {
+                return false;
+            }
+            if (txtLastName.Text == "")
+            {
+                return false;
+            }
+            if (txtInsuranceAmount.Text == "")
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //Button to show summary
+        private void btnSummary_Click(object sender, EventArgs e)
+        {
+            showSummary();
+
+        }
+        // Button to clear current sales person information
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtInsuranceAmount.Text = "";
+        }
+
+        // Button to clear all information
+        private void btnAllClear_Click(object sender, EventArgs e)
+        {
+            showSummary();
+            btnSummary.Controls.Clear();
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtInsuranceAmount.Text = "";
+
+        }
+        // Method to show the information in dialogue box
+        private void showSummary()
+        {
+            MessageBox.Show("Total number of ploicies is " + countTotalPolicies + ". The total dollar insurance sold is " + txtInsuranceAmount.Text + ". Total commsion earned is " + commissionEarned + ".");
+        }
+    }
+}
